45f2bbec29ae57e742aea5ffca68897e4da8a1b8
abdullahbektass <68663760+abdullahbektass@users.noreply.github.com>
2024-12-09 15:20:53 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 
-import FloatingShape from './components/floatingShape';
+import FloatingShape from './components/FloatingShape';
 
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
